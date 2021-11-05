99e9b07ea22c06d5a9c75839897489f855f0edb2
Alberto <albicento.ac@gmail.com>
2021-11-05 12:36:10 +0100

    Refactoring reformatting and plenty of other minor changes

---
+++ README.md
@@ -0,0 +1,13 @@
+# Visual representation of the electric potential field in a capacitor
+
+The concept behind this project is that it's possible to obtain an approximation of the electric potential in a grid of nodes by averaging the potential of a node with the one of its neighbours.
+
+This operation repeated for a good amount of iteration leads to interesting results like the one below.
+
+## Why does the code look like garbage?
+
+This is an old project of mine that I've been working on during high-school.
+
+It's been discontinued for a long time and the code is rather primitive.
+
+I'm trying to improve the code structure in my free time to make it look like it was made in this century.
--- potential.py
+++ potential.py
@@ -1,114 +1,120 @@
-from graphics import *
 import numpy as np
-import time
+import numpy.typing as npt
 
+class Plate:
+    pass
 
 class PotentialGrid:
-    potential = np.zeros((0, 0, 0))
-    locked = np.zeros((0, 0, 0))
-    gridLength = 0
+    """Class representing a 3D grid with a capacitor  inside"""
 
-    def __init__(self, gridLength, cDistance, cLength, cHeight):
-        self.gridLength = gridLength
-        self.potential = np.zeros(
-            (gridLength * 2, gridLength * 2, gridLength * 2))
+    def set_cell(self, x: int, y: int, z: int, value: int = None, lock: bool = False):
+        """Allows to set the potential for a specific point of the grid and eventually lock it"""
+        if x < 0 or y < 0 or z < 0:
+            raise ValueError("One or more coordinates are negative")
+        if value is not None:
+            self.potential[x, y, z] = value
+        self.locked[x, y, z] = int(lock)
+
+    def __init__(self, grid_length: int, distance: int, width: int, height: int):
+        self.grid_length: npt.ArrayLike = grid_length
+        self.potential: npt.ArrayLike = np.zeros(
+            (grid_length * 2, grid_length * 2, grid_length * 2))
         self.locked = np.zeros(
-            (gridLength * 2, gridLength * 2, gridLength * 2))
-        for z in range(0, 2 * gridLength):
-            for x in range(0, 2 * gridLength):
-                for y in range(0, 2 * gridLength):
-                    # condensatore carico positivamente e blocco il potenziale
-                    if x == (gridLength - cDistance) - 1 and y >= (gridLength - cLength) and y <= (gridLength + cLength) - 1 and z >= (gridLength - cHeight) and z <= (gridLength + cHeight):
-                        self.potential[x, y, z] = 10
-                        self.locked[x, y, z] = 1
-                    # condensatore carico negativamente e blocco il potenziale
-                    elif x == (gridLength + cDistance) and y >= (gridLength - cLength) and y <= (gridLength + cLength) - 1 and z >= (gridLength - cHeight) and z <= (gridLength + cHeight):
-                        self.potential[x, y, z] = -10
-                        self.locked[x, y, z] = 1
-                    # blocco il potenziale a 0 al bordo della griglia (distanza infinita dalle piastre)
-                    elif x == 0 or x == (2 * gridLength) - 1 or y == 0 or y == (2 * gridLength) - 1 or z == 0 or z == (2 * gridLength) - 1:
-                        self.locked[x, y, z] = 1
+            (grid_length * 2, grid_length * 2, grid_length * 2))
+        for z in range(0, 2 * grid_length):
+            for x in range(0, 2 * grid_length):
+                for y in range(0, 2 * grid_length):
+                    # positively charged + blocked potential
+                    if x == (grid_length - distance) - 1 and (grid_length - width) <= y <= (
+                            grid_length + width) - 1 and (
+                            grid_length - height) <= z <= (grid_length + height):
+                        self.set_cell(x, y, z, value=10, lock=True)
+                    # negatively charged + blocked potential
+                    elif x == (grid_length + distance) and (grid_length - width) <= y <= (
+                            grid_length + width) - 1 and (
+                            grid_length - height) <= z <= (grid_length + height):
+                        self.set_cell(x, y, z, value=-10, lock=True)
+                    # block edges potential
+                    elif x == 0 or x == (2 * grid_length) - 1 or y == 0 or y == (
+                            2 * grid_length) - 1 or z == 0 or z == (2 * grid_length) - 1:
+                        self.set_cell(x, y, z, lock=True)
 
-    def writePotential(self, fileName, absolute = False):
+    def write_potential(self, file_name: str, absolute: bool = False):
 
-        out = open(fileName, "w")
+        out = open(file_name, "w")
 
-        for x in range(0, 2 * self.gridLength):
-            for y in range(0, 2 * self.gridLength):
-                for z in range(0, 2 * self.gridLength):
-                    potential = str(abs(self.potential[x, y, z])) if absolute else str(self.potential[x, y, z])
-                    out.write(str(x) + " " + str(y) + " " + str(z) + " " +
-                              potential + "\n")
+        for x in range(0, 2 * self.grid_length):
+            for y in range(0, 2 * self.grid_length):
+                for z in range(0, 2 * self.grid_length):
+                    potential = abs(self.potential[x, y, z]) if absolute else self.potential[x, y, z]
+                    out.write(f"{x} {y} {z} {potential} \n")
             out.write("\n")
         out.close()
-    
-    def writeLocked(self, fileName):
-        out = open(fileName, "w")
 
-        for x in range(0, 2 * self.gridLength):
-            for y in range(0, 2 * self.gridLength):
-                for z in range(0, 2 * self.gridLength):
+    def write_locked(self, file_name: str):
+        out = open(file_name, "w")
+
+        for x in range(0, 2 * self.grid_length):
+            for y in range(0, 2 * self.grid_length):
+                for z in range(0, 2 * self.grid_length):
                     if self.locked[x, y, z]:
                         out.write(str(x) + " " + str(y) + " " + str(z) + "\n")
             out.write("\n")
         out.close()
 
-    def writeCapacitor(self, fileName):
-        out = open(fileName, "w")
+    def write_capacitor(self, file_name: str):
+        out = open(file_name, "w")
 
-        for x in range(0, 2 * self.gridLength):
-            for y in range(0, 2 * self.gridLength):
-                for z in range(0, 2 * self.gridLength):
+        for x in range(0, 2 * self.grid_length):
+            for y in range(0, 2 * self.grid_length):
+                for z in range(0, 2 * self.grid_length):
                     if self.locked[x, y, z] and abs(self.potential[x, y, z]) == 10:
                         out.write(str(x) + " " + str(y) + " " + str(z) + "\n")
             out.write("\n")
         out.close()
 
-
-    def update(self, iterationsNumber):
-        iteration = 0
-        while iteration < iterationsNumber:
-            for x in range(0, 2 * self.gridLength):
-                for y in range(0, 2 * self.gridLength):
-                    for z in range(0, 2 * self.gridLength):
+    def update(self, iterations: int):
+        for i in range(iterations):
+            for x in range(2 * self.grid_length):
+                for y in range(2 * self.grid_length):
+                    for z in range(2 * self.grid_length):
                         if self.locked[x, y, z]:
                             pass
                         else:
                             self.potential[x, y, z] = (self.potential[x + 1, y, z] + self.potential[x - 1, y, z] +
-                                                       self.potential[x, y + 1, z] + self.potential[x, y - 1, z] + self.potential[x, y, z + 1] + self.potential[x, y, z - 1]) / 6
-            iteration += 1
+                                                       self.potential[x, y + 1, z] + self.potential[x, y - 1, z] +
+                                                       self.potential[x, y, z + 1] + self.potential[x, y, z - 1]) / 6
 
-    def showGraph2D(self, z): # z è l'altezza a cui viene sezionato il piano tridimensionale
-        winX, winY = 1000, 1000
-        win = GraphWin("Ponteziale elettrico", winX, winY)
-        win.setBackground("black")
-        win.setCoords(-5, -5, (self.gridLength * 2) +
-                      5, (self.gridLength * 2) + 5)
-
-        for x in range(0, 2 * self.gridLength):
-            for y in range(0, 2 * self.gridLength):
-                c = Circle(Point(x, y), .3)
-                c.setOutline("white")
-                c.draw(win)
-                if self.potential[x, y, z] > 0:
-                    c.setFill("orange")
-                if self.potential[x, y, z] > 2:
-                    c.setFill("red")
-                if self.potential[x, y, z] < 0:
-                    c.setFill("cyan")
-                if self.potential[x, y, z] < -2:
-                    c.setFill("blue")
-                if self.locked[x, y, z]:
-                    c.setFill("white")
-
-        while True:
-            time.sleep(5)
+#   def show_section(self, z: int):
+#       win_x, win_y = 1000, 1000
+#       win = GraphWin(f"2D section of the potential at height {z}", win_x, win_y)
+#       win.setBackground("black")
+#       win.setCoords(-5, -5, (self.grid_length * 2) +
+#                     5, (self.grid_length * 2) + 5)
+#
+#       for x in range(0, 2 * self.grid_length):
+#           for y in range(0, 2 * self.grid_length):
+#               c = Circle(Point(x, y), .3)
+#               c.setOutline("white")
+#               c.draw(win)
+#               if self.potential[x, y, z] > 0:
+#                   c.setFill("orange")
+#               if self.potential[x, y, z] > 2:
+#                   c.setFill("red")
+#               if self.potential[x, y, z] < 0:
+#                   c.setFill("cyan")
+#               if self.potential[x, y, z] < -2:
+#                   c.setFill("blue")
+#               if self.locked[x, y, z]:
+#                   c.setFill("white")
+#
+#       while True:
+#           pass
 
 
 ourGrid = PotentialGrid(10, 2, 4, 5)
 ourGrid.update(700)
-ourGrid.writePotential("potential.fas") # la vecchia funzione che aveva scritto lei
-ourGrid.writePotential("abs-potential.fas", True) # stessa cosa ma coi valori assoluti (utile per grafico)
-ourGrid.writeCapacitor("capacitor.fas") # grafico del condensatore nello spazio (senza potenziale)
-ourGrid.writeLocked("locked.fas") # grafico dei punti bloccati nello spazio
-ourGrid.showGraph2D(10) # sezione a metà del cubo per vedere il potenziale in 2d e non buttare del "buon" codice
+ourGrid.write_potential("potential.fas")
+ourGrid.write_potential("abs-potential.fas", absolute=True)
+ourGrid.write_capacitor("capacitor.fas")
+ourGrid.write_locked("locked.fas")
